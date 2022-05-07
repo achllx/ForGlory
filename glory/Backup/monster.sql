e90ebcdad66d6902d3f51bc0b6932ed644e02272
Marchel <103285015+xBaee@users.noreply.github.com>
2022-05-07 12:43:42 +0700

    Delete glory directory

--- glory/Backup/monster.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO monster VALUE (1, "Slime", 1000, 1000, 1, 1, 1, 1, 5, "slime.gif", 50, 50 );
-INSERT INTO monster VALUE (2, "Ghost", 2000, 2000, 5, 5, 5, 5, 15, "ghost.gif", 70, 70 );
-INSERT INTO monster VALUE (3, "SwampDog", 3000, 3000, 5, 5, 10, 10, 25, "dog.gif", 100, 70 );
-INSERT INTO monster VALUE (4, "Ifrit", 5000, 5000, 10, 10, 5, 5, 100, "ifrit.gif", 150, 150 );
-INSERT INTO monster VALUE (5, "BoneDragon", 10000, 5000, 5, 5, 10, 10, 25, "dog.gif", 100, 70 );
-
-INSERT INTO current VALUE (1, "Ghost", 2000, 2000, 5, 5, 5, 5, 15, "ghost.gif", 70, 70 );
-
